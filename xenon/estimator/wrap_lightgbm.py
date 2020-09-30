f0c4a5a5c990d0d7789202fbbf90d50fd55cf645
qichun.tang@xtalpi.com <qichun.tang@xtalpi.com>
2020-09-30 15:30:32 +0800

    fix lgbm multi-clf

--- xenon/estimator/wrap_lightgbm.py
+++ xenon/estimator/wrap_lightgbm.py
@@ -62,7 +62,7 @@ class LGBMEstimator(BaseEstimator):
         self.model = None
         self.current_iterations = 0
         self.early_stopped = False
-        self.logger=get_logger(self)
+        self.logger = get_logger(self)
 
     def fit(self, X, y, X_valid=None, y_valid=None, categorical_feature="auto",
             sample_weight=None, **kwargs):
@@ -101,15 +101,16 @@ class LGBMEstimator(BaseEstimator):
             num_threads=self.n_jobs
         )
         if self.objective == "multiclass":
-            param.update({"num_class": len(set(y))})
+            param.update({"num_class": np.max(y) + 1})
         num_boost_round = self.n_estimators - self.current_iterations
         if num_boost_round <= 0:
             self.logger.warning(f"num_boost_round = {num_boost_round}, <=0, "
-                           f"n_estimators = {self.n_estimators}, "
-                           f"current_iterations = {self.current_iterations}")
+                                f"n_estimators = {self.n_estimators}, "
+                                f"current_iterations = {self.current_iterations}")
             return self
         if self.early_stopped:
-            self.logger.info(f"{self.__class__.__name__} is early_stopped, best_iterations = {self.model.best_iteration}")
+            self.logger.info(
+                f"{self.__class__.__name__} is early_stopped, best_iterations = {self.model.best_iteration}")
             return self
         train_set = lightgbm.Dataset(
             X, y,
